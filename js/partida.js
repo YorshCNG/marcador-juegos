b55b02b4c81f40eef1818e488bbf0461687c769b
YorshCNG <yorshgv2405@gmail.com>
2026-04-01 02:47:47 -0300

    Update partida.js

--- js/partida.js
+++ js/partida.js
@@ -400,7 +400,7 @@ document.getElementById("btnCerrarModal").addEventListener("click", function(){
 
     localStorage.clear();
 
-    window.location.href = "index.html";
+    window.location.href = "/marcador-juegos/index.html";
 
 });}
 

d4f05a48ab52a0dcede7febe5d2f08709bc6e6c0
YorshCNG <yorshgv2405@gmail.com>
2026-04-01 03:27:23 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,26 @@
-const CACHE_NAME = "marcador-juegos-v1";
+const CACHE_NAME = "marcador-juegos-v2";
 
 const urlsToCache = [
+    "/",
+    "/index.html",
 
-"/",
-"/html/partida.html",
-"/html/configurarPartida.html",
-"/html/seleccionarJuego.html",
+    "/html/partida.html",
+    "/html/configurarPartida.html",
+    "/html/seleccionarJuego.html",
 
-"/css/styles.css",
+    "/css/indice.css",
+    "/css/partida.css",
+    "/css/configurarPartida.css",
+    "/css/seleccionarJuego.css",
 
-"/js/app.js",
-"/js/partida.js",
-"/js/configurarPartida.js",
-"/js/seleccionarJuego.js"
+    "/js/app.js",
+    "/js/partida.js",
+    "/js/configurarPartida.js",
+    "/js/seleccionarJuego.js",
 
-];
+    "/icons/icon-192.png",
+    "/icons/icon-512.png"
+];;
 
 self.addEventListener("install", event => {
 
@@ -40,4 +46,18 @@ return response || fetch(event.request);
 
 );
 
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if(key !== CACHE_NAME){
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
 });

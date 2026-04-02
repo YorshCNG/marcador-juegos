379593126d649a5a9d14e0862fda50e699a02603
YorshCNG <yorshgv2405@gmail.com>
2026-04-02 01:42:34 -0300

    Update seleccionarJuego.js

--- js/seleccionarJuego.js
+++ js/seleccionarJuego.js
@@ -2,6 +2,18 @@ function irJuego(juego){
 
     localStorage.setItem("juegoSeleccionado", juego);
 
+    function mostrarToast(mensaje){
+
+    const toast = document.getElementById("toast");
+
+    toast.innerText = mensaje;
+    toast.classList.add("show");
+
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 2000);
+}
+
     window.location.href = "/marcador-juegos/html/configurarPartida.html";
 
 }
